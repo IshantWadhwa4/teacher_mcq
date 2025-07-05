e6582afa138a558bf0c85cd770e9e656987a4a6f
Ishant wadhwa <58129452+IshantWadhwa4@users.noreply.github.com>
2025-07-05 19:01:37 +0530

    Update teacher_app.py

--- teacher_app.py
+++ teacher_app.py
@@ -10,7 +10,7 @@ import base64
 from datetime import datetime
 
 # GitHub configuration
-GITHUB_TOKEN = "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"  # Replace with your new token
+GITHUB_TOKEN = "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"  # Replace with your new token
 GITHUB_REPO = "IshantWadhwa4/data_tsmcq"
 GITHUB_PATH = "questions"  # Path where test files will be stored
 
